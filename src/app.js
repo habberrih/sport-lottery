1853a98d16918648fddf29b75db04739f1d1f9c6
AimanBadi <aimanbadi861@gmail.com>
2022-12-27 20:24:23 +0200

    fixed issues with paths

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-const defualtDirect = path.join(__dirname, '..', 'public');
+const defualtDirect = path.join(__dirname, "..", "public");
 
 app.use(express.json());
 
-app.use('/lottery', express.static(defualtDirect));
+app.use("/lottery", express.static(defualtDirect));
 
 module.exports = app;
