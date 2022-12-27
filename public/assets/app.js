b2e0e3080e2971c0e2c301d606c4f2f6ca70f587
12a02d <abdul2013ly@gmail.com>
2022-12-27 20:40:45 +0200

    updated

--- public/assets/app.js
+++ public/assets/app.js
@@ -6,16 +6,16 @@ var numberSpin = function (selector) {
   var stopValue = 1;
   var spin = function (flag, x) {
     var value = element.style.transform;
-    value = value ? parseFloat(value.split("(")[1].split(")").join("")) : 0;
+    value = value ? parseFloat(value.split('(')[1].split(')').join('')) : 0;
     if (flag && flag == true) {
       if (
         stopValue != 1 &&
         (value <= stopValue || value - section / factor <= stopValue) &&
-        typeof x != "undefined" &&
+        typeof x != 'undefined' &&
         value >= x * -section &&
         value <= (x - 0.5 >= 0 ? x - 0.5 : stopValue == 0 ? 0 : 0.5) * -section
       ) {
-        element.style.transform = "translateY(" + stopValue + "%)";
+        element.style.transform = 'translateY(' + stopValue + '%)';
         stopValue = 1;
         return true;
       }
@@ -25,12 +25,12 @@ var numberSpin = function (selector) {
           : 0;
     }
     if (value && value <= -(section * num)) {
-      element.style.transform = "translateY(" + section + "%)";
+      element.style.transform = 'translateY(' + section + '%)';
       value = 0;
     } else {
       value -= section / factor;
     }
-    element.style.transform = "translateY(" + value + "%)";
+    element.style.transform = 'translateY(' + value + '%)';
     return false;
   };
   var spinTimer = setInterval(spin, 10);
@@ -51,16 +51,23 @@ var numberSpin = function (selector) {
   };
 };
 
-let slots = [...document.querySelectorAll(".slot")];
-let btn = document.querySelector("#btn");
-btn.addEventListener("click", () => {
-  if (slots[0].classList.contains("visible")) {
-    numberSpin("scroll1").stop(5000, Math.floor(Math.random() * 2 + 1));
-    numberSpin("scroll2").stop(6000, Math.floor(Math.random() * 6));
-    numberSpin("scroll3").stop(7000, Math.floor(Math.random() * 10));
+let slots = [...document.querySelectorAll('.slot')];
+let btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+  if (slots[0].classList.contains('visible')) {
+    firstNumber = Math.floor(Math.random() * 2 + 1);
+    secondNumber = Math.floor(Math.random() * 6);
+    thirdNumber = Math.floor(Math.random() * 10);
+
+    if (secondNumber === 6) {
+      thirdNumber = 0;
+    }
+    numberSpin('scroll1').stop(5000, firstNumber);
+    numberSpin('scroll2').stop(6000, secondNumber);
+    numberSpin('scroll3').stop(7000, thirdNumber);
   } else {
     slots.map((slot) => {
-      slot.classList.add("visible");
+      slot.classList.add('visible');
     });
   }
 });
